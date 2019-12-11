f67fd7e3b02ee1fbaf719ec0aeab168e0869bbc7
asus <aminnabavi2@yahoo.com>
2019-12-11 14:29:35 +0330

    Add project files.

---
+++ WindowsFormsApp1/Forms/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.تعریفپایهToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.کالاToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.کاربرToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.عملیاتToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ثبتسفارشToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.جمعکلفاکتورToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.شارژحسابToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.تنظیماتToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ویرایشاطلاعاتToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.رنگپسزمینهToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.خروجToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.تعریفپایهToolStripMenuItem,
+            this.عملیاتToolStripMenuItem,
+            this.تنظیماتToolStripMenuItem,
+            this.خروجToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(156, 450);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // تعریفپایهToolStripMenuItem
+            // 
+            this.تعریفپایهToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.کالاToolStripMenuItem,
+            this.کاربرToolStripMenuItem});
+            this.تعریفپایهToolStripMenuItem.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.تعریفپایهToolStripMenuItem.Name = "تعریفپایهToolStripMenuItem";
+            this.تعریفپایهToolStripMenuItem.Size = new System.Drawing.Size(143, 27);
+            this.تعریفپایهToolStripMenuItem.Text = "تعریف پایه";
+            // 
+            // کالاToolStripMenuItem
+            // 
+            this.کالاToolStripMenuItem.Name = "کالاToolStripMenuItem";
+            this.کالاToolStripMenuItem.Size = new System.Drawing.Size(121, 28);
+            this.کالاToolStripMenuItem.Text = "کالا";
+            // 
+            // کاربرToolStripMenuItem
+            // 
+            this.کاربرToolStripMenuItem.Name = "کاربرToolStripMenuItem";
+            this.کاربرToolStripMenuItem.Size = new System.Drawing.Size(121, 28);
+            this.کاربرToolStripMenuItem.Text = "کاربر";
+            // 
+            // عملیاتToolStripMenuItem
+            // 
+            this.عملیاتToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ثبتسفارشToolStripMenuItem,
+            this.جمعکلفاکتورToolStripMenuItem,
+            this.شارژحسابToolStripMenuItem});
+            this.عملیاتToolStripMenuItem.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.عملیاتToolStripMenuItem.Name = "عملیاتToolStripMenuItem";
+            this.عملیاتToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+            this.عملیاتToolStripMenuItem.Text = "عملیات";
+            
+            // 
+            // ثبتسفارشToolStripMenuItem
+            // 
+            this.ثبتسفارشToolStripMenuItem.Name = "ثبتسفارشToolStripMenuItem";
+            this.ثبتسفارشToolStripMenuItem.Size = new System.Drawing.Size(183, 26);
+            this.ثبتسفارشToolStripMenuItem.Text = "ثبت سفارش";
+            // 
+            // جمعکلفاکتورToolStripMenuItem
+            // 
+            this.جمعکلفاکتورToolStripMenuItem.Name = "جمعکلفاکتورToolStripMenuItem";
+            this.جمعکلفاکتورToolStripMenuItem.Size = new System.Drawing.Size(183, 26);
+            this.جمعکلفاکتورToolStripMenuItem.Text = "جمع کل فاکتور";
+            // 
+            // شارژحسابToolStripMenuItem
+            // 
+            this.شارژحسابToolStripMenuItem.Name = "شارژحسابToolStripMenuItem";
+            this.شارژحسابToolStripMenuItem.Size = new System.Drawing.Size(183, 26);
+            this.شارژحسابToolStripMenuItem.Text = "شارژ حساب";
+            // 
+            // تنظیماتToolStripMenuItem
+            // 
+            this.تنظیماتToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ویرایشاطلاعاتToolStripMenuItem,
+            this.رنگپسزمینهToolStripMenuItem});
+            this.تنظیماتToolStripMenuItem.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.تنظیماتToolStripMenuItem.Name = "تنظیماتToolStripMenuItem";
+            this.تنظیماتToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+            this.تنظیماتToolStripMenuItem.Text = "تنظیمات";
+            // 
+            // ویرایشاطلاعاتToolStripMenuItem
+            // 
+            this.ویرایشاطلاعاتToolStripMenuItem.Name = "ویرایشاطلاعاتToolStripMenuItem";
+            this.ویرایشاطلاعاتToolStripMenuItem.Size = new System.Drawing.Size(194, 26);
+            this.ویرایشاطلاعاتToolStripMenuItem.Text = "ویرایش اطلاعات";
+            // 
+            // رنگپسزمینهToolStripMenuItem
+            // 
+            this.رنگپسزمینهToolStripMenuItem.Name = "رنگپسزمینهToolStripMenuItem";
+            this.رنگپسزمینهToolStripMenuItem.Size = new System.Drawing.Size(194, 26);
+            this.رنگپسزمینهToolStripMenuItem.Text = "رنگ پس زمینه";
+            // 
+            // خروجToolStripMenuItem
+            // 
+            this.خروجToolStripMenuItem.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.خروجToolStripMenuItem.Name = "خروجToolStripMenuItem";
+            this.خروجToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+            this.خروجToolStripMenuItem.Text = "خروج";
+            this.خروجToolStripMenuItem.Click += new System.EventHandler(this.خروجToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem تعریفپایهToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem کالاToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem کاربرToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem عملیاتToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ثبتسفارشToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem جمعکلفاکتورToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem شارژحسابToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem تنظیماتToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ویرایشاطلاعاتToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem رنگپسزمینهToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem خروجToolStripMenuItem;
+    }
+}
+
